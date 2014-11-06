657f7008e3d5fd671a1a4dbffe408aa8b40aa1b9
Willem Mulder <willemmulder@gmail.com>
2014-11-06 19:29:40 +0100

    First commit!

---
+++ README.md
@@ -0,0 +1,52 @@
+# Diont for Cordova
+
+Easy Service Discovery on Local Networks. This Cordova plugin discovers services that are announced by Diont servers on the local network (wifi), either by other mobile devices that use this Diont plugin, or by a [Nodejs Diont](https://github.com/willemmulder/Diont) server.
+
+
+## Features
+* 100% complete: no extra dependencies
+* allows for transmitting extra, arbitrary service information
+* includes examples
+* also [available](https://github.com/willemmulder/Diont) for [Nodejs](http://nodejs.org)
+
+## Thanks
+Thanks to Gramakri for his [Cordova Datagram plugin](https://github.com/gramakri/cordova-plugin-datagram) that served as the inspiration for this plugin.
+
+## Get started
+```javascript
+var diont = Diont();
+
+// ======
+// Listen for announcements and renouncements in services
+// ======
+diont.on("serviceAnnounced", function(serviceInfo) {
+	// A service was announced
+	// This function triggers for services not yet available in diont.getServiceInfos()
+	// serviceInfo is an Object { isOurService : Boolean, service: Object }
+	// service.name, service.host and service.port are always filled
+	console.log("A new service was announced", serviceInfo.service);
+	// List currently known services
+	console.log("All known services", diont.getServiceInfos());
+});
+
+diont.on("serviceRenounced", function(serviceInfo) {
+	console.log("A service was renounced", serviceInfo.service);
+	console.log("All known services", diont.getServiceInfos());
+});
+
+// ======
+// Announce our own service
+// ======
+var service = {
+	name: "TestServer 1",
+	host: "127.0.0.1", // when omitted, defaults to the local IP
+	port: "1231"
+	// any additional information is allowed and will be propagated
+};
+diont.announceService(service);
+
+// Renounce after 5 seconds
+setTimeout(function() {
+	diont.renounceService(service);
+}, 5000);
+```
---
+++ src/android/Diont.java
@@ -0,0 +1,120 @@
+package net.willemmulder.diont.cordova;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.util.SparseArray;
+
+public class Diont extends CordovaPlugin {
+
+    SparseArray<MulticastSocket> sockets;
+    SparseArray<SocketListener> listeners;
+
+    public Diont() {
+        sockets = new SparseArray<MulticastSocket>();
+        listeners = new SparseArray<SocketListener>();
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+        
+        final int instanceId = data.getInt(0);
+        MulticastSocket socket = sockets.get(instanceId);
+
+        if (action.equals("init")) {
+            if (socket == null) {
+                try {
+                    String host = data.getString(1);
+                    int port = data.getInt(2);
+
+                    socket = new MulticastSocket(port);
+                    socket.setTimeToLive(1); // Local network
+                    socket.joinGroup(InetAddress.getByName(host)); // Tell the OS to listen for messages on the specified host and treat them as if they were meant for this host
+                    Boolean disableLoopback = false;
+                    socket.setLoopbackMode(disableLoopback);
+
+                    sockets.put(instanceId, socket);
+                    callbackContext.success();
+                } catch (Exception e) {
+                    callbackContext.error(e.toString());
+                }
+            }
+        } else if (action.equals("listen")) {
+            try {
+                // Set up listener
+                SocketListener listener = new SocketListener(socket, callbackContext);
+                listeners.put(instanceId, listener);
+                listener.start();
+            } catch (Exception e) {
+                callbackContext.error(e.toString());
+            }
+        }  else if (action.equals("send")) {
+            String message = data.getString(1);
+            String host = data.getString(2);
+            int port = data.getInt(3);
+
+            try {
+                byte[] bytes = message.getBytes("UTF-8");
+                DatagramPacket packet = new DatagramPacket(bytes, bytes.length, InetAddress.getByName(host), port);
+                socket.send(packet);
+                callbackContext.success(message);
+            } catch (IOException ioe) {
+                callbackContext.error("IOException: " + ioe.toString());
+            }
+        } else if (action.equals("close")) {
+            if (socket != null) {
+                socket.close();
+                sockets.remove(instanceId);
+                SocketListener listener = listeners.get(instanceId);
+                if (listener != null) {
+                    listener.interrupt();
+                    listeners.remove(instanceId);
+                }
+            }
+            callbackContext.success();
+        } else {
+            return false; // 'MethodNotFound'
+        }
+        return true;
+    }
+
+    private class SocketListener extends Thread {
+
+        MulticastSocket socket;
+        PluginResult result;
+
+        public SocketListener(MulticastSocket socket, CallbackContext callbackContext) {
+            this.socket = socket;
+        }
+
+        public void run() {
+            byte[] data = new byte[2048];
+            DatagramPacket packet = new DatagramPacket(data, data.length);
+            while (true) {
+                try {
+                    this.socket.receive(packet);
+                    String msg = new String(data, 0, packet.getLength(), "UTF-8")
+                                    .replace("'", "\'")
+                                    .replace("\r", "\\r")
+                                    .replace("\n", "\\n");
+
+                    result = new PluginResult(PluginResult.Status.OK, msg);
+                    result.setKeepCallback(true); // Allow for additional callbacks
+                    callbackContext.sendPluginResult(result);
+
+                } catch (Exception e) {
+                    return;
+                }
+            }
+        }
+    }
+
+}
+
